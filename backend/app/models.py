bed20895d69a7c6941f9a2dddb12507fa7c119dd
defnvary <defnvary@gmail.com>
2025-11-22 00:35:35 +0530

    use hashed passwords

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,9 +1,18 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+
+    # hash and store the password
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    # check / verify given password matches the hashed one
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
     def __repr__(self):
         return f"<User {self.email}>"
